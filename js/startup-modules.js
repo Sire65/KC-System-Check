94ce1d8475d4bcd7cef99eb3b769feb8c2a393a1
Sire65 <ha-joko@web.de>
2026-09-08 12:21:24 +0200

    Robuste LIVE-Aufteilung laden

--- js/startup-modules.js
+++ js/startup-modules.js
@@ -13,4 +13,4 @@ import"./delivery-proof.js";
 import"./action-progress.js";
 import"./remote-operations.js";
 import"./monitoring-runs-sync.js";
-import"./live-layout.js";
+import"./live-layout-v2.js";
